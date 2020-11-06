7d1c0ea86874c7434c5de93f8196acdc1286d7b1
Romer <romerlucas@gmail.com>
2020-11-05 21:48:31 -0300

    estado anterior do disable_astrology

--- bot/client.py
+++ bot/client.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 from bot.akinator.akinator_game import AkinatorGame
-from bot.astrology.astrology_chart import AstrologyChart
+#from bot.astrology.astrology_chart import AstrologyChart
 from bot.chess.chess import Chess
 from bot.chess.puzzle import Puzzle
 from bot.sww_leaderboard.leaderboard import Leaderboard
@@ -14,13 +14,13 @@ load_dotenv()
 
 client = discord.Client()
 
-client = commands.Bot(command_prefix=os.environ.get("BOT_PREFIX", 'cp!'))
+client = commands.Bot(command_prefix=os.environ.get("BOT_PREFIX", 'sp!'))
 
 chess_bot = Chess()
 chess_bot.load_games()
 
-astrology_bot = AstrologyChart()
-astrology_bot.load_charts()
+#astrology_bot = AstrologyChart()
+#astrology_bot.load_charts()
 
 puzzle_bot = Puzzle()
 
--- run.py
+++ run.py
@@ -5,12 +5,13 @@ from dotenv import load_dotenv
 from bot import client
 from bot.akinator_cmds import *
 from bot.anime_cmds import *
-from bot.astrology_cmds import *
+#from bot.astrology_cmds import *
 from bot.bot import *
 from bot.chess_cmds import *
 from bot.level import *
 from bot.sww_cmds import *
+from bot.jogo_da_velha import *
 
 load_dotenv()
 
-client.run(os.environ.get("API_KEY"))
+client.run(os.environ.get("API_KEYs"))
